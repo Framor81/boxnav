ad6d71df4dc1ed3f46a5096babdbf43d65d179e5
anthonyjclark <anthonyjclark@gmail.com>
2023-07-13 17:18:19 -0700

    Cleaned argparse

--- boxsim.py
+++ boxsim.py
@@ -1,5 +1,6 @@
-import math
 from argparse import ArgumentParser, Namespace
+from math import radians
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 from celluloid import Camera
@@ -16,10 +17,10 @@ boxes = [
     Box(Pt(-185, 1250), Pt(420, 1250), Pt(420, -350), Pt(10, 650)),
     Box(Pt(-1110, 775), Pt(420, 775), Pt(420, 450), Pt(-835, 650)),
     Box(Pt(-910, 100), Pt(-910, 775), Pt(-750, 775), Pt(-820, 200)),
-    Box(Pt(-750, 340), Pt(-750, 100), Pt(-4800, 100), Pt(-4650, 150)),
-    Box(Pt(-4750, 340), Pt(-4480, 340), Pt(-4480, -2200), Pt(-4600, -2000)),
-    Box(Pt(-4480, -1935), Pt(-4480, -2200), Pt(-6450, -640), Pt(-5700, -2000)),
-    Box(Pt(-5525, -2200), Pt(-5830, -2200), Pt(-5830, 3025), Pt(-5780, 2550)),
+    # Box(Pt(-750, 340), Pt(-750, 100), Pt(-4800, 100), Pt(-4650, 150)),
+    # Box(Pt(-4750, 340), Pt(-4480, 340), Pt(-4480, -2200), Pt(-4600, -2000)),
+    # Box(Pt(-4480, -1935), Pt(-4480, -2200), Pt(-6450, -640), Pt(-5700, -2000)),
+    # Box(Pt(-5525, -2200), Pt(-5830, -2200), Pt(-5830, 3025), Pt(-5780, 2550)),
     # Box(Pt(-5525, 2800), Pt(-5525, 2300), Pt(-4600, 2300), Pt(-6100, 2600)),
 ]
 
@@ -30,7 +31,7 @@ def simulate(args: Namespace, dataset_path: str) -> None:
     box_world = BoxEnv(boxes)
 
     initial_position = Pt(0, 0)
-    initial_rotation = math.radians(90)
+    initial_rotation = radians(90)
 
     if args.navigator == "wandering":
         NavigatorConstructor = WanderingNavigator
@@ -43,48 +44,52 @@ def simulate(args: Namespace, dataset_path: str) -> None:
         initial_position, initial_rotation, box_world, allow_out_of_bounds=args.ue
     )
 
-    # Wrap the agent in a UE wrapper if we're using UE
+    # Wrap the agent if we want to connect to Unreal Engine
     if args.ue:
         agent = UENavigatorWrapper(
             agent,
-            dataset_path,
-            py_server_port=args.py_port,
-            ue_server_port=args.ue_port,
-            save_images=args.collect,
+            args.save_images,
+            args.py_port,
+            args.ue_port,
         )
 
     fig, ax = plt.subplots()
     camera = Camera(fig)
     while not agent.at_final_target() and agent.num_actions_taken() < args.max_actions:
         try:
-            action_taken, correct_action = agent.take_action()
+            _, _ = agent.take_action()
         except TimeoutError as e:
             print(e)
-            agent.ue5.close_osc()
+            if isinstance(agent, UENavigatorWrapper):
+                agent.ue5.close_osc()
             raise SystemExit
 
-        if args.anim_type:
+        if args.anim_ext:
             box_world.display(ax)
             agent.display(ax, box_world.scale)
             ax.invert_xaxis()
             camera.snap()
 
-    if args.ue:
+    if isinstance(agent, UENavigatorWrapper):
         agent.ue5.close_osc()
 
-    if agent.at_final_target():
-        print(
-            f"Simulation complete, it took {agent.num_actions_taken()} actions to reach the end."
-        )
-    else:
-        print(
-            f"Agent was not able to reach target within {agent.num_actions_taken()} actions."
-        )
+    print("Simulation complete.", end=" ")
 
-    if args.anim_type:
-        print("Saving animation...")
+    num_actions = agent.num_actions_taken()
+    if agent.at_final_target():
+        print(f"Agent reached final target in {num_actions} actions.")
+    else:
+        print(f"Agent was unable to reach final target within {num_actions} actions.")
+
+    if args.anim_ext:
+        output_filename = None
+        num = 1
+        while not output_filename or Path(output_filename).exists():
+            output_filename = f"output_{num}.{args.anim_ext}"
+            num += 1
         anim = camera.animate()
-        anim.save("output." + args.anim_type)
+        anim.save(output_filename)
+        print(f"Animation saved to {output_filename}.")
 
 
 def main():
@@ -102,7 +107,7 @@ def main():
     # Optional arguments
     #
 
-    argparser.add_argument("--anim_type", type=str, help="Output format for animation.")
+    argparser.add_argument("--anim_ext", type=str, help="Output format for animation.")
 
     argparser.add_argument(
         "--max_actions", type=int, default=50, help="Maximum allowed actions."
@@ -141,15 +146,11 @@ def main():
     if args.save_images and not args.ue:
         raise ValueError("Cannot collect data without connecting to Unreal Engine.")
 
-    if (args.py_port or args.ue_port) and not args.ue:
-        raise ValueError("Ports are not necessary without Unreal Engine.")
-
     if args.resolution and not args.ue:
         raise ValueError("Resolution is unnecessary without Unreal Engine.")
 
-    simulate(args, args.save_path)
+    simulate(args, args.save_images)
 
 
 if __name__ == "__main__":
     main()
-    print("Done")
